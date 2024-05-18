f491f1f5efebcd9d38e1bb4e252b0231b8bd4ad7
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-18 12:58:07 +0500

    adding 21 day of challenge

---
+++ Question 62/app.js
@@ -0,0 +1,10 @@
+// *Question 62:* Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.
+// Filling in the blueprint with an example student
+var student = {
+    name: "Muhammad saleem",
+    age: 21,
+    class: "Bs (graduation)"
+};
+// Showing the student's information
+console.log(student);
+// We're using the blueprint to make sure our student has a name, age, and list of courses.
